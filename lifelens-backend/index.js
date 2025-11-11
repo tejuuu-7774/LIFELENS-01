ccdefefd6bebff62c7fd35e3c11f77b279db0848
Palwai Tejaswini <palwaitejaswini7777@gmail.com>
2025-11-11 22:32:23 +0530

    Few changes in dashboard logic.

--- lifelens-backend/index.js
+++ lifelens-backend/index.js
@@ -19,4 +19,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
